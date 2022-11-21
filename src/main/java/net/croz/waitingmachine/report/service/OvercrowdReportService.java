9810c96a404738433e8521584d0ff3271cbf20b1
Sanjin Kurelic <skurelic@croz.net>
2022-11-21 13:59:48 +0100

    Add custom metric to scheduler

--- src/main/java/net/croz/waitingmachine/report/service/OvercrowdReportService.java
+++ src/main/java/net/croz/waitingmachine/report/service/OvercrowdReportService.java
@@ -1,5 +1,8 @@
 package net.croz.waitingmachine.report.service;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.croz.waitingmachine.queue.rest.WaitingQueueRestRepository;
@@ -7,18 +10,29 @@ import net.croz.waitingmachine.status.model.QueueStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OvercrowdReportService {
 
     private final WaitingQueueRestRepository queueRepository;
+    private final MeterRegistry meterRegistry;
+
+    private static final String OVERCROWD_SITUATION = "overcrowd_situation";
+
+    @PostConstruct
+    public void init() {
+        Counter.builder(OVERCROWD_SITUATION).description("Some test").register(meterRegistry);
+    }
 
     @Scheduled(fixedRate = 1000)
     public void checkForOvercrowdedQueue() {
         var peopleInWaitingList = queueRepository.findAllByStatus(QueueStatus.WAITING);
 
         if (peopleInWaitingList.size() > 10) {
+            Metrics.counter(OVERCROWD_SITUATION).increment();
             log.error("We have overcrowded queue here");
         }
     }
